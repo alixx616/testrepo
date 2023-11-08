e24e13255321a57de720309c28833dfdf77d3f6e
haseeb a ali <haseebaali@gmail.com>
2023-11-08 10:56:04 -0600

    haali-functions

---
+++ code/functions.R
@@ -0,0 +1,16 @@
+###Some Custom Functions
+
+# Calculate Circle Circumference
+circumference <- function(r){
+  pi*r^2
+}
+
+# Calculate Circle area
+area <- function(r){
+  2*pi*r
+}
+
+# Add two numbers
+sum_two_nums <- function(x, y){
+  x + y
+}
