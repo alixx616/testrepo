26372182543795c2bb1bf882c063ef1a616af052
Haseeb A Ali <haseebaali@gmail.com>
2023-11-08 10:31:28 -0600

    some functions

---
+++ code/functions.R
@@ -0,0 +1,12 @@
+### Some user defined functions
+
+# Generate length of circumference given radius
+circumference <- function(r){
+  2*pi*r
+}
+print(circumference(2))
+
+# Sum two numbers
+sum_two_nums <- function(x, y){
+  x + y
+}
